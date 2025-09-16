6b466c5084ab4d9ca680b471a7bce4f871e6cf53
v-filgueiras <vfilgueiras.dev@gmail.com>
2025-09-16 10:14:31 -0400

    criando estrutura do projeto

---
+++ src/main/java/com/eletronic_system_store/eletronic_store_system/EletronicStoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.eletronic_system_store.eletronic_store_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicStoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicStoreSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletronic_system_store/eletronic_store_system/EletronicStoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletronic_system_store.eletronic_store_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletronicStoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
