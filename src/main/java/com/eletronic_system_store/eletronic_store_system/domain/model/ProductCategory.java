b37ef9d229d02134c24af204398d113baf8471df
v-filgueiras <vfilgueiras.dev@gmail.com>
2025-09-16 11:02:03 -0400

    criando categoria de produtos

---
+++ src/main/java/com/eletronic_system_store/eletronic_store_system/domain/model/ProductCategory.java
@@ -0,0 +1,10 @@
+package com.eletronic_system_store.eletronic_store_system.domain.model;
+
+public enum ProductCategory {
+    SMARTPHONES,
+    COMPUTERS,
+    ACCESSORIES,
+    AUDIO,
+    GAMING,
+    STORAGE;
+}
